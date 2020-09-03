d468123af8d888501d85713aa42f60aa01db5f27
Jad <jadsamaha98@gmail.com>
2020-09-03 14:41:49 +0000

    my calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,22 +8,23 @@ def main():
 if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print('1')
-    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        exit(1)
+    elif argv[2] != '+' and argv[2] 
+    != '-' and argv[2] != '*' and argv[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-        print('1')
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     if argv[2] == '+':
         print('{} + {} = {}'.format(a, b, add(a, b)))
-        print('0')
+        exit(0)
     elif argv[2] == '-':
         print('{} - {} = {}'.format(a, b, sub(a, b)))
-        print('0')
+        exit(0)
     elif argv[2] == '*':
         print('{} * {} = {}'.format(a, b, mul(a, b)))
-        print('0')
+        exit(0)
     elif argv[2] == '/':
         print('{} / {} = {}'.format(a, b, div(a, b)))
-        print('0')
+        exit(0)
     main()
