49ea877ce181d4bca29cb6dd34f2acd897f7f909
Jad <jadsamaha98@gmail.com>
2020-09-08 20:55:24 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary.keys() is None:
         return None
     maximum = max(a_dictionary.values())
     for k, v in a_dictionary.items():
