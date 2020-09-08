77a191629004fd8051454862fbfdec14c714038f
Jad <jadsamaha98@gmail.com>
2020-09-08 20:00:24 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
-    sorted_dict = sorted (a_dictionary.keys())
+    sorted_dict = sorted(a_dictionary.keys())
     return sorted_dict[-1]
