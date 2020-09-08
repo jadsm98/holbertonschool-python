880f63bfaed2057139d31bdbac289dfebc11be22
Jad <jadsamaha98@gmail.com>
2020-09-08 15:53:40 +0000

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,5 +4,5 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
         return a_dictionary
     else:
-        a_dictionary.update(key = value)
+        a_dictionary[key] = value
         return a_dictionary
