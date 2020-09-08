cde4e2ab262ef30e49fb7d33f6c7b70ccb4a5bc4
Jad <jadsamaha98@gmail.com>
2020-09-08 14:48:54 +0000

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
     size = len(matrix)
-    new_matrix = [[0 for x in range(size) for y in range(size)]
+    new_matrix = [[0 for x in range(size)] for y in range(size)]
     for i, row in enumerate(matrix):
         for j, col in enumerate(row):
             new_matrix[i][j] = col*col
