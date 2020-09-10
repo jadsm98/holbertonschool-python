eb367a62b5740390c823dd532dacbac192cda986
Jad <jadsamaha98@gmail.com>
2020-09-10 17:46:58 +0000

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        ans = fct(*args)
+        return ans
+    except Exception as err:
+        print("Exception: ", err, file=stderr)
+        return None
