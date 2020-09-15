bc46c3de43cd7281d398c0866c664829f43e91ee
Jad <jadsamaha98@gmail.com>
2020-09-15 09:51:51 +0000

    square6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Square6"""
+
+
+class Square:
+    """coordinate of a square"""
+    def __init__(self, size=0, position=(0,0)):
+        """init"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.__size
+
+    @property
+    def position(self):
+        """getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter for position"""
+        if type(value) is not tuple or len(value) != 2 or all(i < 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area"""
+        return self.__size*self.__size
+
+    def my_print(self):
+        """prints square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print(' '*self.__position[0], end='')
+                for j in range(self.__size):
+                    print('{}'.format('#'), end='')
+                print()
