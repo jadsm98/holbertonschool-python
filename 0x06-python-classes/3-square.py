2dda67e6852e0ec5e379d8231fc7888167ef71a1
Jad <jadsamaha98@gmail.com>
2020-09-15 07:49:15 +0000

    square3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""square3"""
+
+
 class Square:
+    """Area of a square"""
     def __init__(self, size=0):
         self.__size = size
         if not type(size) is int:
@@ -7,4 +11,5 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
     def area(self):
+        """Area function"""
         return self.__size*self.__size
