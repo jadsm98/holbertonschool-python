e5483b45aa6d56c23c984c255177f464d345d0b9
Jad <jadsamaha98@gmail.com>
2020-09-15 11:21:11 +0000

    square6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print('\n'*self.__position[1], end='')
+            for i in range(self.__position[1]):
+                print('')
             for i in range(self.__size):
                 print(' '*self.__position[0], end='')
                 for j in range(self.__size):
