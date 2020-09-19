2ed9aad35add9b57c8d24feff9ec5c0460b06f57
Jad <jadsamaha98@gmail.com>
2020-09-19 07:59:48 +0000

    matmul

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -31,7 +31,7 @@ def matrix_mul(m_a, m_b):
         raise TypeError("each row of m_b must be of the same size")
     if not len(m_a[0]) == len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    result = [[0 for i in m_b[0]] for j in m_a] 
+    result = [[0 for i in m_b[0]] for j in m_a]
     for i in range(len(m_a)):
         for j in range(len(m_b[0])):
             for k in range(len(m_b)):
