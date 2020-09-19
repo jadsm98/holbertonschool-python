9e4131008230cb4e359b5485b2b1308a6d2310c0
Jad <jadsamaha98@gmail.com>
2020-09-19 08:18:31 +0000

    matrix div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,8 +10,6 @@ def matrix_divided(matrix, div):
     and divides the elements by this number and
     returns them in a new matrix """
 
-    if div == float('inf') or div == -float('inf') or div != div:
-        div = 10
     divided_matrix = []
     for i in matrix:
         if type(i) is not list or type(matrix) is not \
@@ -24,5 +22,8 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    divided_matrix = [[round(i/div, 2) for i in j] for j in matrix]
+    if div == float('inf') or div == -float('inf'):
+        divided_matrix = [[0.0 for i in j] for j in matrix]
+    else:
+        divided_matrix = [[round(i/div, 2) for i in j] for j in matrix]
     return divided_matrix
