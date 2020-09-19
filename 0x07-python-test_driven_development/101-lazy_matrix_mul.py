e5244e791a5f718b9383707c91ccfb5e879cc786
Jad <jadsamaha98@gmail.com>
2020-09-19 08:07:20 +0000

    numpy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,14 +6,15 @@ Lazy matrix multiplication module
 
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """ Takes 2 matrices and returns their
         multiplication if possible using
         NumPy """
     if type(m_a) is not list:
-        raise ValueError("m_a can only be of type 'list'")
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
     if type(m_b) is not list:
-        raise ValueError("m_b can only be of type 'list'")
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
     if not all(type(i) is list for i in m_a):
         raise ValueError("m_a can only be a matrix")
     if not all(type(i) is list for i in m_b):
@@ -23,14 +24,14 @@ def lazy_matrix_mul(m_a, m_b):
     if m_b in [[], [[]]]:
         raise ValueError("m_b cannot be of dimension 0")
     if not all(all(type(j) in [int, float] for j in i) for i in m_a):
-        raise ValueError("m_a should be a matrix of integers")
+        raise ValueError("invalid data type for einsum")
     if not all(all(type(j) in [int, float] for j in i) for i in m_b):
-        raise ValueError("m_b should be a matrix of integers")
+        raise ValueError("invalid data type for einsum")
     if not all(len(i) == len(m_a[0]) for i in m_a):
-        raise TypeError("can't multiply sequence by non-int of type 'list'")
+        raise TypeError("setting an array element with a sequence")
     if not all(len(i) == len(m_b[0]) for i in m_b):
-        raise TypeError("can't multiply sequence by non-int of type 'list'")
+        raise TypeError("setting an array element with a sequence")
     if not len(m_a[0]) == len(m_b):
-        raise ValueError('size of m_a is different than size of m_b')
-                
+        raise ValueError("size of m_a is different than size of m_b")
+
     return np.matmul(m_a, m_b)    
