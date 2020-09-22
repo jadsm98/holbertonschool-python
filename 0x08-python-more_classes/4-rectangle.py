071c0f32baa081da74385df4c91696818826b59e
Jad <jadsamaha98@gmail.com>
2020-09-22 09:31:06 +0000

    rectangle4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -9,13 +9,16 @@ class Rectangle:
         self.width = width
         self.height = height
 
+
     @property
     def width(self):
         return self.__width
 
+
     @property
     def height(self):
         return self.__height
+
 
     @width.setter
     def width(self, value):
@@ -25,6 +28,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
@@ -33,13 +37,16 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+
     def area(self):
         return self.__width*self.__height
+
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2*(self.__width + self.__height)
+
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
@@ -53,6 +60,7 @@ class Rectangle:
                 for j in range(self.__width):
                     rep += '#'
         return rep
+
 
     def __repr__(self):
         return "Rectangle(" + str(self.__width) + ', ' + str(self.__height) + ")"
