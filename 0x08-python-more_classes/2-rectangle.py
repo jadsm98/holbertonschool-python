0da7f2f428f9e78a08c61ae9bab60107d07c6f31
Jad <jadsamaha98@gmail.com>
2020-09-22 09:09:27 +0000

    rectangle2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -39,4 +39,4 @@ class Rectangle:
         return self.__width*self.__height
 
     def perimeter(self):
-        return 2*(self.__width + self.__height)
+        return 2*self.__width + 2*self.__height
