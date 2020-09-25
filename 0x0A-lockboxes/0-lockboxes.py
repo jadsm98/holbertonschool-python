6d1f380e434844b1d55b6ddb0e4bdb370bf2ee42
Jad <jadsamaha98@gmail.com>
2020-09-25 13:43:54 +0000

    lockboxes

---
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module Lockboxes"""
+
+def canUnlockAll(boxes):
+    """ function to check if all
+    boxes in a list are unlocked """
+
+    boxesDict = {}
+    boxesDict[0] = False  # unlocked
+    for i in range(len(boxes) - 1):
+        boxesDict[i + 1] = True  # locked
+    print(boxesDict)
+
+    while True:
+        change = False
+        for i, box in enumerate(boxes):
+            if not boxesDict[i]:
+                for key in box:
+                    if boxesDict[key]:
+                        print("unlocked box: {}".format(key))
+                        boxesDict[key] = False
+                        change = True
+        if not change:
+            break
+
+    for box, lockStatus in boxesDict.items():
+        if lockStatus:
+            return False
+    return True
+
