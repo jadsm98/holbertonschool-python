7b1d505f34b60254b1e37725bd77e6e383cd955a
Jad <jadsamaha98@gmail.com>
2020-09-28 06:49:44 +0000

    lockboxes

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -3,32 +3,23 @@
 
 
 def canUnlockAll(boxes):
-    """ function to check if all
-    boxes in a list are unlocked:
-    >>>canUnlockAll([[1], [2], [3], [4], []])
-    True
-    >>>canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
-    False
-    >>>canUnlockAll(45)
-    Traceback (most recent call last):
-    TypeError: boxes should be a list of lists
-    """
+    """function that opens boxes"""
+
 
     boxesDict = {}
     boxesDict[0] = False
     for i in range(len(boxes) - 1):
         boxesDict[i + 1] = True
-    unlocked_boxes = []
     while True:
-        new_list = [i for i in unlocked_boxes]
-        unlocked_boxes = []
-        for box, lockStatus in boxesDict.items():
-            unlocked_boxes.append(box)
-        if new_list == unlocked_boxes:
+        change = False
+        for i, box in enumerate(boxes):
+            if not boxesDict[i]:
+                for key in box:
+                    if boxesDict[key]:
+                        boxesDict[key] = False
+                        change = True
+        if not change:
             break
-        for b in unlocked_boxes:
-            for k in boxes[b]:
-                boxesDict[k] = False
     for box, lockStatus in boxesDict.items():
         if lockStatus:
             return False
