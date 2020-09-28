b50fafacbb7275a0487ae6f9400d7822cb542346
Jad <jadsamaha98@gmail.com>
2020-09-28 12:17:38 +0000

    lockboxes

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -6,12 +6,16 @@ def canUnlockAll(boxes):
     """function that opens boxes"""
 
     unlocked = [0]
-    for loop in range(2):
+    while True:
+        change = False
         for i, box in enumerate(boxes):
             if i in unlocked:
                 for key in box:
                     if key not in unlocked:
                         unlocked.append(key)
+                        change = True
+        if not change:
+            break
     if len(unlocked) == len(boxes):
         return True
     return False
