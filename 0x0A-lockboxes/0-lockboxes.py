1956469abd39d227b7b08a8dc8c5c111f4382896
Jad <jadsamaha98@gmail.com>
2020-09-28 07:31:30 +0000

    lockboxes

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -5,21 +5,19 @@
 def canUnlockAll(boxes):
     """function that opens boxes"""
 
-    boxesDict = {}
-    boxesDict[0] = False
-    for i in range(len(boxes) - 1):
-        boxesDict[i + 1] = True
+    if not all(type(i) is list for i in boxes) or not type(boxes) is list:
+        raise TypeError("boxes must be a list of lists")
+    unlocked = [0]
     while True:
         change = False
-        for i in range(len(boxes)):
-            if not boxesDict[i]:
-                for key in boxes[i]:
-                    if boxesDict[key]:
-                        boxesDict[key] = False
+        for i, box in enumerate(boxes):
+            if i in unlocked:
+                for key in box:
+                    if key not in unlocked:
+                        unlocked.append(key)
                         change = True
         if not change:
             break
-    for box, lockStatus in boxesDict.items():
-        if lockStatus:
-            return False
-    return True
+    if len(unlocked) == len(boxes):
+        return True
+    return False
