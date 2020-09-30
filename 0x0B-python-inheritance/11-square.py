b8601b3fd9a9ae761462faf8cbece76340e42a2c
Jad <jadsamaha98@gmail.com>
2020-09-30 16:26:41 +0000

    inheritance

---
+++ 0x0B-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module square2"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+        Rectangle.integer_validator(self, "size", self.__size)
+
+    def area(self):
+        return self.__size*self.__size
+
+    def __repr__(self):
+        return "[Square]" + str(width) + "/" + str(height)
