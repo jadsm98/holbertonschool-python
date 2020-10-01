6c03ba8a64ac451b9768ab0a04a4c40fb287f614
Jad <jadsamaha98@gmail.com>
2020-10-01 17:11:27 +0000

    json

---
+++ 0x0C-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module save_to_json_file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function save_to_json_file"""
+
+    with open(filename, "w+") as f:
+        return f.write(json.dumps(my_obj))
