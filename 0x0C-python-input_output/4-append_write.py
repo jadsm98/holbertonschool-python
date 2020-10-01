274c8a6a9455543e66d7199768dbd170068531e9
Jad <jadsamaha98@gmail.com>
2020-10-01 17:00:07 +0000

    write file

---
+++ 0x0C-python-input_output/4-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module append_write"""
+
+
+def append_write(filename="", text=""):
+    """function append_write"""
+
+    with open(filename, "a+") as f:
+        return f.append(text)
