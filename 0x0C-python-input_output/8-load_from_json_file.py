e8cfd5150a4537e294008bae9fae19e09b730ac2
Jad <jadsamaha98@gmail.com>
2020-10-01 18:25:14 +0000

    json

--- 0x0C-python-input_output/8-load_from_json_file.py
+++ 0x0C-python-input_output/8-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """function load_from_json_file"""
 
-    with open(filename, "r+") as f:
+    with open(filename, "w+") as f:
         return json.loads(f.read())
