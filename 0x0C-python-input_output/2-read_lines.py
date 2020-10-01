be0abdf8f2210de093ef13f57c03ef8c36c26c01
Jad <jadsamaha98@gmail.com>
2020-10-01 20:47:40 +0000

    read n lines

--- 0x0C-python-input_output/2-read_lines.py
+++ 0x0C-python-input_output/2-read_lines.py
@@ -8,8 +8,8 @@ def read_lines(filename="", nb_lines=0):
     with open(filename) as f:
         num = sum(1 for line in f)
         if nb_lines <= 0 or nb_lines >= num:
-            for lines in f:
-                print(lines, end="")
+            for line in f:
+                print(line, end="")
         else:
-            for lines in range(nb_lines):
-                print(f.readline(), end="")
+            head = [next(f) for x in range(nb_lines)]
+            print(''.join(head))
