85a64beb8c1a3f7b7724e1b798c59da110aff2cb
Jad <jadsamaha98@gmail.com>
2020-10-06 09:45:14 +0000

    base

---
+++ 0x0D-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""base module"""
+
+
+class Base:
+    """class Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
