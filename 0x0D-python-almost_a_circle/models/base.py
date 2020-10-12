03861db657cc74ebc3221bfc1e80f06739128f5c
Jad <jadsamaha98@gmail.com>
2020-10-12 06:19:51 +0000

    almost a circle

--- 0x0D-python-almost_a_circle/models/base.py
+++ 0x0D-python-almost_a_circle/models/base.py
@@ -10,7 +10,10 @@ class Base:
     """class Base"""
 
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """initialization"""
+
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
