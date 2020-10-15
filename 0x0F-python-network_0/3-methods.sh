618eb279563436cf2f56346f16f3b522148ae678
Jad <jadsamaha98@gmail.com>
2020-10-15 19:14:01 +0000

    put

--- 0x0F-python-network_0/3-methods.sh
+++ 0x0F-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #comment
-curl "$1" -i -sX OPTIONS | grep -i Allow | awk '{$1="";print}' | sed -e 's/^[[:space:]]*//'
+curl "$1" -sX OPTIONS | grep -i Allow | awk '{$1="";print}' | sed -e 's/^[[:space:]]*//'
