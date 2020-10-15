2d9ea66903bfc9b926693c32c24e2296c068d43d
Jad <jadsamaha98@gmail.com>
2020-10-15 17:55:42 +0000

    py

--- 0x0F-python-network_0/6-peak.py
+++ 0x0F-python-network_0/6-peak.py
@@ -3,7 +3,9 @@
 
 
 def find_peak(list_of_integers):
+    """function"""
+
     for i in range(len(list_of_integers)):
-        if list_of_integers[i] >= list_of_integers[i - 1] and 
+        if list_of_integers[i] >= list_of_integers[i - 1] and\
            list_of_integers[i] >= list_of_integers[i + 1]:
             return list_of_integers[i]
