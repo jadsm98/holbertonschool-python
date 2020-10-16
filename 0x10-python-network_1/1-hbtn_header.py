4c0cccc6569c79a61772575d0c430de6af313d1c
Jad <jadsamaha98@gmail.com>
2020-10-16 13:33:46 +0000

    header

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     import urllib.request
     import sys
 
-
     with urllib.request.urlopen(sys.argv[1]) as resp:
         res = dict(resp.info())
         print(res.get('X-Request-Id'))
