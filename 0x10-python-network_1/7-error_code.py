17d1ebf23146ff00b2c796cd2dea3de3d992723f
Jad <jadsamaha98@gmail.com>
2020-10-16 16:25:31 +0000

    error2

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
 
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
-        print ('Error code: {}'.format(r.status_code))
+        print('Error code: {}'.format(r.status_code))
     else:
         print(r.text)
