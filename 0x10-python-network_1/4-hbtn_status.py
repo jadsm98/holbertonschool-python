3eb27b31a275645f4ce019c2ec15b909458c31aa
Jad <jadsamaha98@gmail.com>
2020-10-16 15:07:39 +0000

    status2

--- 0x10-python-network_1/4-hbtn_status.py
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -9,4 +9,3 @@ if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     print('Body response:\n\t- type: {}\n\t- contents: {}'\
           .format(type(r.text), r.text))
-
