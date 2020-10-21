63c2729026874e6762616ef098717370910e7493
Jad <jadsamaha98@gmail.com>
2020-10-21 18:53:24 +0000

    temp1

--- 0x12-SQL_introduction/102-top_city.sql
+++ 0x12-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Temperatures #1
 -- Commands
-SELECT city, AVG(value) 'avge_temp' FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avge_temp DESC LIMIT 3;
+SELECT city, AVG(value) 'avg_temp' FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avge_temp DESC LIMIT 3;
