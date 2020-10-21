9dd11add18603b649fb0cf0cd1eeda70d27fd65e
Jad <jadsamaha98@gmail.com>
2020-10-21 19:05:03 +0000

    to utf8mb4

--- 0x12-SQL_introduction/100-move_to_utf8.sql
+++ 0x12-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,7 @@
 -- Go to UTF-8
 -- command
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- command
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- command
 ALTER TABLE first_table CHANGE name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
