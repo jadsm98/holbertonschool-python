79cee74d34c2f71120803c2ea6206eda66dc7fbb
Jad <jadsamaha98@gmail.com>
2020-10-21 17:05:53 +0000

    average

--- 0x12-SQL_introduction/14-average.sql
+++ 0x12-SQL_introduction/14-average.sql
@@ -1,6 +1,3 @@
 -- Average
 -- Command
-ALTER TABLE second_table ADD average FLOAT;
-SET @avge = AVG(score);
-INSERT INTO second_table (average) VALUES (avge);
-SELECT average FROM second_table;
+SELECT AVG(score) 'average' FROM second_table;
