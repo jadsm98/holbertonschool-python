0c507227af9ad11da60412e8e047da6ecbac03dd
Jad <jadsamaha98@gmail.com>
2020-10-23 10:00:50 +0000

    create and read

--- 0x13-SQL_more_queries/1-create_user.sql
+++ 0x13-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Command
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- command
-GRANT ALL PRIVILEGES ON 'database'.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
