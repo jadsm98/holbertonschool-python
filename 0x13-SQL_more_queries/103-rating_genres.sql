a07d211b019354f2b47bb9582c5137017a6d7267
Jad <jadsamaha98@gmail.com>
2020-10-23 20:06:03 +0000

    rating genres

---
+++ 0x13-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,3 @@
+-- Best genre
+-- commands
+SELECT tg.name, SUM(tsr.rate) 'rating' FROM tv_genres AS tg LEFT OUTER JOIN tv_show_genres AS tsg ON tsg.genre_id = tg.id LEFT OUTER JOIN tv_show_ratings AS tsr ON tsr.show_id = tsg.show_id GROUP BY tg.name ORDER BY rating DESC;
