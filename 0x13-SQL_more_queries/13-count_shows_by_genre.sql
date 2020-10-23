352c8eb688741a9e0f9d2cab75cda27eaabf3625
Jad <jadsamaha98@gmail.com>
2020-10-23 17:14:58 +0000

    count shows

--- 0x13-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x13-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Number of shows by genre
 -- Command
-SELECT tv_genres.name 'genre', COUNT(tv_show_genres.genre_id) 'number_of_shows' FROM tv_genres LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name 'genre', COUNT(tv_show_genres.genre_id) 'number_of_shows' FROM tv_genres LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_show_genres.genre_id <> '0' GROUP BY genre ORDER BY number_of_shows DESC;
