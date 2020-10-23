c3fafa40861afcb528a2c165d141e02ee7c5d2f1
Jad <jadsamaha98@gmail.com>
2020-10-23 09:17:26 +0000

    privileges

--- 0x13-SQL_more_queries/0-privileges.sql
+++ 0x13-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- My privileges
 -- Command
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Command
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
