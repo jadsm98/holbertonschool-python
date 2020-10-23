b6f30964e35b5eec00fa06dc004a012e3c71b3db
Jad <jadsamaha98@gmail.com>
2020-10-23 12:41:02 +0000

    cities of cali

--- 0x13-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x13-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of Cali
 -- Command
-SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id DESC;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
