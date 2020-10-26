371adb7c0efdbb880aec6e25a324db2a448a5238
Jad <jadsamaha98@gmail.com>
2020-10-26 14:55:17 +0000

    N queens

---
+++ 0x14-nqueens/0-nqueens.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""module"""
+
+
+from sys import argv
+if len(argv) > 2:
+    print('Usage: nqueens N')
+    exit(1)
+N = argv[1]
+if not type(N) is int:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
+def printSolution(board):
+    ls = []
+    for i, v in enumerate(board):
+        for j in range(len(v)):
+            if board[i][j] == 1:
+                ls.append([i, j])
+    print(ls)
+
+
+def isSafe(board, row, col):
+    for i in range(col):
+        if board[row][i]:
+            return False
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if board[i][j]:
+            return False
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while j >= 0 and i < N:
+        if board[i][j]:
+            return False
+        i = i + 1
+        j = j - 1
+    return True
+
+
+def solveNQUtil(board, col):
+    if col == N:
+        printSolution(board)
+        return True
+    res = False
+    for i in range(N):
+        if isSafe(board, i, col):
+            board[i][col] = 1
+            res = solveNQUtil(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+
+def solveNQ():
+    board = [[0 for j in range(N)]
+             for i in range(N)]
+    if not solveNQUtil(board, 0):
+        print("Solution does not exist")
+        return
+    return
+
+
+solveNQ()
