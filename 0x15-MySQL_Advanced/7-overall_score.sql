4b6a1740309bbeb573cee2bf9a66b213f11a8ced
Jad <jadsamaha98@gmail.com>
2020-10-28 21:24:30 +0000

    scores

--- 0x15-MySQL_Advanced/7-overall_score.sql
+++ 0x15-MySQL_Advanced/7-overall_score.sql
@@ -3,6 +3,6 @@
 DELIMITER //
 CREATE PROCEDURE ComputeOverallScoreForUser (IN user_id INT)
 BEGIN
-UPDATE users SET overall_score = (SELECT AVG(score) FROM corrections AS c WHERE c.user_id = user_id GROUP BY c.user_id) WHERE id = user_id;
+UPDATE users AS u LEFT OUTER JOIN corrections as c ON c.user_id = u.id SET overall_score = (SELECT AVG(score) FROM corrections AS c WHERE c.user_id = user_id GROUP BY c.user_id) WHERE id = user_id;
 END; //
 DELIMITER ;
