b6468c6e45239efcfe936aa1d124192e6efdc515
Jad <jadsamaha98@gmail.com>
2020-10-28 14:33:10 +0000

    store

--- 0x15-MySQL_Advanced/4-store.sql
+++ 0x15-MySQL_Advanced/4-store.sql
@@ -1,4 +1,4 @@
 -- Buy
 -- Commands
-CREATE TRIGGER trig AFTER INSERT ON items
-FOR EACH ROW UPDATE items SET quantity = quantity - NEW.quantity;
+CREATE TRIGGER trig AFTER INSERT ON orders
+FOR EACH ROW UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
