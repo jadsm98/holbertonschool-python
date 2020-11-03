7e290f3cfc7940a0fddb41a7cb3c37ffd47ea053
Jad <jadsamaha98@gmail.com>
2020-11-03 18:47:25 +0000

    all py doc

--- 0x17-NoSQL/8-all.py
+++ 0x17-NoSQL/8-all.py
@@ -5,6 +5,7 @@
 def list_all(mongo_collection):
     """ function"""
 
+    find = mongo_collection.find({})
     if mongo_collection == {}:
         return []
-    return [doc for doc in mongo_collection]
+    return [doc for doc in find]
