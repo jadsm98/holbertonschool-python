1c376c979cd97520392b58ba3f8da180b5008cf7
Jad <jadsamaha98@gmail.com>
2020-11-03 19:58:39 +0000

    tpic

---
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """function"""
+
+    x = mongo_collection.find({topic: {$in: topics}}
+    return x.get("name")
