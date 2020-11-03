2e18d7a76f3f5ed8a9a36471229413328026f821
Jad <jadsamaha98@gmail.com>
2020-11-03 20:08:36 +0000

    topic

--- 0x17-NoSQL/11-schools_by_topic.py
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Module"""
+"""module"""
 
 
 def schools_by_topic(mongo_collection, topic):
     """function"""
 
-    x = mongo_collection.find({topic: {$in: "topics"}})
+    x = mongo_collection.find({topic: {$in: [topics]}})
     return x.get("name")
