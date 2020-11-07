6b59c8e5837f6cf66cf78a67d14c4a9e355e2151
Jad <jadsamaha98@gmail.com>
2020-11-07 23:26:39 +0000

    ex0

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -4,6 +4,7 @@
 
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -15,7 +16,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, float, int]) -> str:
         """method"""
         ran = str(uuid.uuid1())
         self._redis.set(ran, data)
