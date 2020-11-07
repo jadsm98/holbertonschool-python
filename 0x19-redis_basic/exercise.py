549b2c4554bf6aa799358d9f141bdddd45255ba5
Jad <jadsamaha98@gmail.com>
2020-11-07 23:15:56 +0000

    ex0

---
+++ 0x19-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+import redis
+import uuid
+
+
+class Cache:
+    """this is a class"""
+
+
+    def __init__(self):
+        """init"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        """method"""
+        ran = str(uuid.uuid1())
+        self._redis.set(ran, data)
+        return self._redis.get(ran).decode("utf-8")
