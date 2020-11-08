fb8385f82f91a2c718581c0b57acc00362fb06c5
Jad <jadsamaha98@gmail.com>
2020-11-08 21:46:21 +0000

    ex0

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -5,17 +5,17 @@
 import redis
 import uuid
 
+
 class Cache:
     """this is a class"""
-
 
     def __init__(self):
         """init"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data) -> str:
         """method"""
-        ran = str(uuid.uuid1())
+        ran = int(uuid.uuid1())
         self._redis.set(ran, data)
-        return self._redis.get(ran)
+        return self._redis.get(ran).decode("utf-8")
