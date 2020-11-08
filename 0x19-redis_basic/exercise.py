8e1cdb4178ad001263ed25a13141972435676d3d
Jad <jadsamaha98@gmail.com>
2020-11-08 21:50:13 +0000

    ex0

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -12,9 +12,10 @@ class Cache:
     def __init__(self):
         """init"""
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data) -> str:
         """method"""
         ran = uuid.uuid1().bytes
         self._redis.set(ran, data)
-        return self._redis.get(ran)
+        return data
