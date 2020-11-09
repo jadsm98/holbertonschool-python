cca50a199e907957a56167db307cc78632f93a5e
Jad <jadsamaha98@gmail.com>
2020-11-09 12:48:30 +0000

    exercise

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -4,19 +4,22 @@
 
 import redis
 import uuid
-from typing import Union
-
+from typing import Union, Optional
 
 class Cache:
     """this is a class"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """init"""
-        self._redis = redis.Redis(db=0)
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, int, bytes]) -> str:
+    def store(self, data: Union[str, int, float, bytes]) -> str:
         """method"""
-        id = uuid.uuid1()
-        self._redis.set(id.int, data)
-        return str(id)
+        id = uuid.uuid4()
+        key = str(self._redis.set(id.int, data))
+        return key
+
+    def get(self, key: str, fn: Optional[callable] = None) -> Union[str, int, float, bytes]:
+        """method"""
+        return fn(self._redis.get(key))
