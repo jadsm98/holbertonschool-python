e505523385e31a8701e72e022a01473b77657e9f
Jad <jadsamaha98@gmail.com>
2020-11-11 15:59:39 +0000

    fifo

---
+++ 0x1A-caching/1-fifo_cache.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Module FIFO cache
+"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Method init
+        """
+        super().__init__()
+        self.data = {}
+        self.nextin = 0
+        self.nextout = 0
+
+    def append(self, key):
+        """
+        Append Method
+        """
+        self.nextin += 1
+        self.data[self.nextin] = key
+
+    def pop(self):
+        """
+        Pop method
+        """
+        self.nextout += 1
+        result = self.data.get(self.nextout)
+        del self.cache_data[self.data.get(self.nextout)]
+        del self.data[self.nextout]
+        print('DISCARD: {}'.format(result))
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            self.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.pop()
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+
+        if key not in self.cache_data or key is None:
+            return None
+        return self.cache_data.get(key)
