c676168a3bd6e0e2eb06a2246ea4411a5ca3b8c0
Jad <jadsamaha98@gmail.com>
2020-11-11 13:37:32 +0000

    basic_cache

--- 0x1A-caching/0-basic_cache.py
+++ 0x1A-caching/0-basic_cache.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""module"""
+"""
+Basic_caching module
+"""
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """class"""
+    """
+    Basic class
+    """
     
     def put(self, key, item):
-        """method"""
+        """
+        Add an item in the cache
+        """
+        
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """method"""
+        """
+        Get an item by key
+        """
+
         if key not in self.cache_data or key is None:
             return None
         return self.cache_data.get(key)
