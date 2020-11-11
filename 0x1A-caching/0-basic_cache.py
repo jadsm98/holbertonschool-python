2f4f045a4be0b70360320116089a4d531dd5c5f3
Jad <jadsamaha98@gmail.com>
2020-11-11 16:37:45 +0000

    baisc

--- 0x1A-caching/0-basic_cache.py
+++ 0x1A-caching/0-basic_cache.py
@@ -28,7 +28,4 @@ class BasicCache(BaseCaching):
         """
         Get an item by key
         """
-
-        if key not in self.cache_data or key is None:
-            return None
         return self.cache_data.get(key)
