d33aa3d7a2589e92453a1541c55917d3a1a1496d
Jad <jadsamaha98@gmail.com>
2020-11-13 12:57:10 +0000

    island perimiter

--- 0x1B-island_perimeter/0-island_perimeter.py
+++ 0x1B-island_perimeter/0-island_perimeter.py
@@ -9,10 +9,15 @@ def island_perimeter(grid):
     Function to return the perimeter
     of an island
     """
+    if len(grid) > 100 or any(len(i) for i in grid) > 100:
+        return
 
     perim = 0
     for i, v in enumerate(grid):
         for j in range(len(v)):
+            if grid[0][j] != 0 or grid[i][0] != 0 \
+               or grid[-1][j] != 0 or grid[i][-1] != 0:
+                return
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 0:
                     perim += 1
