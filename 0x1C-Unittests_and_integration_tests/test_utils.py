6b4266e789b208260d334d9ea083038605ca6c98
Jad <jadsamaha98@gmail.com>
2020-11-17 15:47:38 +0000

    test4

--- 0x1C-Unittests_and_integration_tests/test_utils.py
+++ 0x1C-Unittests_and_integration_tests/test_utils.py
@@ -83,8 +83,9 @@ class TestMemoize(unittest.TestCase):
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method', return_value=42) as mock_a_method:
-            TestClass().a_property
-            TestClass().a_property
+            test = TestClass()
+            test.a_property
+            test.a_property
 
-            self.assertEqual(TestClass().a_property, 42)
+            self.assertEqual(test.a_property, 42)
             mock_a_method.assert_called_once()
