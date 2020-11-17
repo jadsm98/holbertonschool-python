635cfd4e8e03e42d688d6240c13a65839866e8e5
Jad <jadsamaha98@gmail.com>
2020-11-17 14:24:18 +0000

    test3

--- 0x1C-Unittests_and_integration_tests/test_utils.py
+++ 0x1C-Unittests_and_integration_tests/test_utils.py
@@ -45,7 +45,7 @@ class TestGetJson(unittest.TestCase):
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
-        ("http://holberton.io" {"payload": False})
+        ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
         """
