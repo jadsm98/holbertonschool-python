07b2f8251c6e96fe6b2d8b68be2e2ff83eda3028
Jad <jadsamaha98@gmail.com>
2020-11-18 15:01:33 +0000

    test_client

--- 0x1C-Unittests_and_integration_tests/test_client.py
+++ 0x1C-Unittests_and_integration_tests/test_client.py
@@ -36,3 +36,42 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient(test.get('repos_url'))
             self.assertEqual(client._public_repos_url, mock_org.return_value.get('repos_url'))
             mock_org.assert_called_once()
+
+    @patch('get_json', return_value=[{'name': 'google'}])
+    def test_public_repos(self, mock_org):
+        """
+        test method
+        """
+
+        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock_get:
+            mock_get.return_value = 'https://github.com/google'
+            test = GithubOrgClient('google')
+            ret = test.public_repos()
+            self.assertEqual(ret, ['google'])
+            mock_org.assert_called_once()
+            mock_get.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license_key, expected_return):
+        """ test GithubOrgClient.has_license"""
+        test_client = GithubOrgClient("twitter")
+        test_return = test_client.has_license(repo, license_key)
+        self.assertEqual(expected_return, test_return)
+
+
+@parameterized.expand([
+    ])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ class TestIntegrationGithubOrgClient """
+    @classmethod
+    def setUpClass(cls):
+        """set up class"""
+        cls.get_patcher = patch('requests.get', side_effect=HTTPError)
+
+    @classmethod
+    def tearDownClass(cls):
+        """tear down class"""
+        cls.get_patcher.stop()
